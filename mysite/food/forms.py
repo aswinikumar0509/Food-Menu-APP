6d67f7997bf93811e2216ca5b60f447b58e40fd9
Aswini Kumar <aswini0509@gmail.com>
2024-06-13 02:34:27 +0530

    update-form and models

--- mysite/food/forms.py
+++ mysite/food/forms.py
@@ -1,2 +1,10 @@
 from django import forms
-from .models import Items
+from .models import Item
+
+
+class ItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ["item_name","item_desc","item_price","item_image"]
+        
--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import Users
+from django.urls import reverse
 
 # Create your models here.
+
+class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+    
+    user_name = models.ForeignKey(Users,on_delete=models.CASCADE , default=1)
+
+    item_name = models.CharField(max_length=100)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500 , default=" ")
+
+    # delete this afterwards
+
+    def get_absolute_url(self):
+        return reverse("food:details" , kwargs={"pk":self.pk})
+    
+
+
 
